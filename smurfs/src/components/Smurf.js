be71401ad0c85f7d33d2a07b72dfca1ede532af1
Pav0l <pavol.karas@gmail.com>
2019-03-08 10:24:03 +0100

    Create Smurf card

--- smurfs/src/components/Smurf.js
+++ smurfs/src/components/Smurf.js
@@ -1,17 +1,32 @@
 import React, { Component } from 'react';
-// import styled from 'styled-components';
+import styled from 'styled-components';
 
 
 export default class Smurf extends Component {
   render() {
+    const { name, age, height, image } = this.props.smurf;
+
     return (
-      <div>
-        <div>{this.props.smurf.name}</div>
-        <div>{this.props.smurf.age}</div>
-        <div>{this.props.smurf.height}</div>
-      </div>
+      <StyledContainer>
+        <img src={image} alt={`${name} img`} />
+        <SmurfName>{name}</SmurfName>
+        <div>Age: {age}</div>
+        <div>Height: {height}</div>
+      </StyledContainer>
     );
   }
 }
 
+const StyledContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  border: 2px solid black;
+  border-radius: 4px;
+  padding: 10px;
+  max-width: 200px;
+  margin: 10px;
+`;
 
+const SmurfName = styled.div`
+  font-weight: 1000;
+`;
