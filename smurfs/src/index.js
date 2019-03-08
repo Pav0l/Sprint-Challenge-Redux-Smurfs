aeae3630ee4117fb03af759cda2731bf9cba670b
Pav0l <pavol.karas@gmail.com>
2019-03-08 21:26:43 +0100

    Remove logger

--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
 import thunk from 'redux-thunk';
-import logger from 'redux-logger';
+// import logger from 'redux-logger';
 import { createStore, applyMiddleware, compose } from 'redux';
 import { Provider } from 'react-redux';
 import rootReducer from './reducers';
@@ -11,7 +11,7 @@ import rootReducer from './reducers';
 const store = createStore(
   rootReducer,
   compose(
-    applyMiddleware(thunk, logger),
+    applyMiddleware(thunk),
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
   )
 );
